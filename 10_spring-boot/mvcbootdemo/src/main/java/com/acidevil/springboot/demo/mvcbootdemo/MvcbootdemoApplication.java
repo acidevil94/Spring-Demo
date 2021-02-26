7d64568557d2fb8411881ab519d410c292204ef3
Andrea Coluzzi <a.coluzzi@mariniimpianti.it>
2021-02-26 16:35:15 +0100

    init spring boot application

---
+++ 10_spring-boot/mvcbootdemo/src/main/java/com/acidevil/springboot/demo/mvcbootdemo/MvcbootdemoApplication.java
@@ -0,0 +1,13 @@
+package com.acidevil.springboot.demo.mvcbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcbootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcbootdemoApplication.class, args);
+	}
+
+}
---
+++ 10_spring-boot/mvcbootdemo/src/test/java/com/acidevil/springboot/demo/mvcbootdemo/MvcbootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acidevil.springboot.demo.mvcbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcbootdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
